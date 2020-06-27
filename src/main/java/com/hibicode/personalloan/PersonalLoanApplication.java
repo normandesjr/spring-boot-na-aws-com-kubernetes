5827e1c6b8da1678484e6833fddec303cf7e86ee
normandesjuniorzup <normandes.junior@zup.com.br>
2020-06-27 11:44:16 -0300

    First commit

---
+++ src/main/java/com/hibicode/personalloan/PersonalLoanApplication.java
@@ -0,0 +1,13 @@
+package com.hibicode.personalloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibicode/personalloan/PersonalLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibicode.personalloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
