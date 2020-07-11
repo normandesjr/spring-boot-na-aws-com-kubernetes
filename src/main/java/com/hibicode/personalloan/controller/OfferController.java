95dabacb03d2b438dcae2a886ceb8c9753c42819
nojemoj <NormandesJoseMoreiraJunior1@correio.itau.com.br>
2020-07-11 18:18:32 -0300

    Update pom

--- src/main/java/com/hibicode/personalloan/controller/OfferController.java
+++ src/main/java/com/hibicode/personalloan/controller/OfferController.java
@@ -2,6 +2,8 @@ package com.hibicode.personalloan.controller;
 
 import com.hibicode.personalloan.controller.resource.OfferRequest;
 import com.hibicode.personalloan.service.OfferService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,6 +14,8 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RequestMapping("/offers")
 public class OfferController {
 
+    private static final Logger logger = LoggerFactory.getLogger(OfferController.class);
+
     private OfferService offerService;
 
     public OfferController(OfferService offerService) {
@@ -21,6 +25,7 @@ public class OfferController {
     @PostMapping
     @ResponseStatus(CREATED)
     public void create(@RequestBody @Valid OfferRequest offerRequest) {
+        logger.info("[OfferController][create] - creating offer: " + offerRequest);
         offerService.save(offerRequest.toDomain());
     }
 
